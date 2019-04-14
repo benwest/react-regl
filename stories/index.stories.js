0b8c404d6d68bcdd43f9024a60aa1c4dd7124be5
Kev Zettler <kevzettler@gmail.com>
2019-04-14 14:54:13 -0700

    kill babel and storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,64 +1,19 @@
 import React from 'react';
+
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
-import Regl, { Draw } from '../src/';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 
-const colorRange = {
-  range: true,
-  min: 0,
-  max: 1,
-  step: 0.1
-};
+import { Button, Welcome } from '@storybook/react/demo';
 
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Infrastructure', module)
-  .addDecorator(withKnobs)
-  .add('Regl Component', () => {
-    return (
-      <Regl width={number('Width', 400)}
-            height={number('Height', 200)}
-            color={[
-              number('R', 0.40625, colorRange),
-              number('G', 0.94921, colorRange),
-              number('B', 0.996, colorRange),
-              1]}
-      />
-    );
-  })
-  .add('Draw Component', () => {
-    return (
-      <Regl
-        width={window.innerWidth}
-        height={window.innerHeight}
-        color={[0,0,0,1]}
-        >
-        <Draw
-          vert={`
-          precision mediump float;
-          attribute vec2 position;
-          void main () {
-            gl_Position = vec4(position, 0, 1);
-          }`}
-
-          frag={`
-          precision mediump float;
-          uniform vec4 color;
-          void main () {
-            gl_FragColor = color;
-          }`}
-
-          attributes={{
-            position: [
-              [-1, 0],
-              [0, -1],
-              [1, 1]
-            ]
-          }}
-          uniforms={{
-            color: [1, 0, 0, 1]
-          }}
-          count={3}
-          />
-      </Regl>
-    )
-  });
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
